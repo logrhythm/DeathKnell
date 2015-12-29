207b6694564c44e48f2f26d9e5bd37593afbb515
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2015-12-29 15:54:34 -0700

    Added code coverage Added unique build number from git commit numbers library is shared library will have unique number

--- scripts/buildRpm.sh
+++ scripts/buildRpm.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 set -e
-
 PACKAGE=DeathKnell
-VERSION=1.0
+
+if [[ $# -ne 1 ]] ; then
+    echo 'Usage:  sh buildRpm <BUILD_TYPE>'
+    echo '        BUILD_TYPE is PRODUCTION or COVERAGE'
+    exit 0
+fi
+ 
+VERSION="$1"
+BUILDSTRING=
+if [ "$1" = "PRODUCTION" ] ; then
+   BUILD_TYPE="-DUSE_DEBUG_COVERAGE=OFF"
+   BUILDSTRING="production"
+elif  [ "$1" = "COVERAGE" ] ; then
+   BUILD_TYPE="-DUSE_DEBUG_COVERAGE=ON"
+   BUILDSTRING="debug"
+else
+   echo "<BUILD_TYPE> must be one of: PRODUCTION or COVERAGE"
+   exit 0
+fi
+
+
+# As version number we use the commit number on HEAD 
+# we do not bother with other branches for now
+GIT_VERSION=`git rev-list HEAD --count`
+VERSION="1.$GIT_VERSION"
+
+echo "Building $PACKAGE, type: $BUILD_TYPE, version: $VERSION"
 
 PWD=`pwd`
 CWD=$PWD/$PACKAGE
@@ -17,7 +42,9 @@ tar czf $PACKAGE-$VERSION.tar.gz ./*
 mkdir -p ~/rpmbuild/SOURCES
 cp $PACKAGE-$VERSION.tar.gz ~/rpmbuild/SOURCES
 cd ~/rpmbuild
-rpmbuild -v -bb --target=x86_64 ~/rpmbuild/SPECS/$PACKAGE.spec
+
+
+rpmbuild -v -bb  --define="version ${VERSION}" --define="buildtype {$BUILD_TYPE}" --define="buildstring {$BUILDSTRING}" --target=x86_64 ~/rpmbuild/SPECS/$PACKAGE.spec
 
 # Copy the artifacts to the local distribution directory
 rm -rf $DISTDIR
@@ -25,3 +52,4 @@ mkdir -p $DISTDIR/include/
 cp -r ~/rpmbuild/BUILD/$PACKAGE/src/*.h $DISTDIR/include
 mkdir -p $DISTDIR/lib/
 cp -r ~/rpmbuild/BUILD/$PACKAGE/*.so $DISTDIR/lib
+
